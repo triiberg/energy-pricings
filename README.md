bcc541aba81fdae0b9b17cf7bbb918dc25821db1
Toomas Riiberg <toomas@merit.ee>
2021-08-25 12:59:27 +0300

    30 minutes of simple proxy service

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # energy-pricings
+
+Just get Estonian electricity prices. Organize the payload in a way that IoT device does not bother itself with date/time functions. See the IoT project as well.
---
+++ app.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const axios = require('axios');
+const moment = require('moment');
+
+const eleringFormatter = function (data) {
+  var parsedData = [];
+  data.forEach(function(d) {
+    var oneLine = {
+      t: moment(d.timestamp*1000).format("HH"),
+      p: (d.price / 10).toFixed(1)
+    }
+    parsedData.push(oneLine);
+  });
+
+  return parsedData;
+
+}
+
+const requestListener = function (req, res) {
+
+  // from current hour and total of 4
+  let startDate = moment().utc().add(-1, 'h').format();
+  let endDate = moment().utc().add(3, 'h').format();
+
+  // final URL like: "https://dashboard.elering.ee/api/nps/price?start=2021-08-22T21:00:00.000Z&end=2021-08-23T20:59:59.999Z"
+  let dataUrl = "https://dashboard.elering.ee/api/nps/price?start=" + startDate + "&end=" + endDate;
+
+  axios.get(dataUrl)
+    .then(function (eleringResponse) {
+
+      let final = {
+        updated: moment().format("HH:mmddD")
+      }
+
+      if(eleringResponse.data.success != true) {
+        final.status = "e1";
+      } else {
+        final.status = "ok";
+      }
+
+      if(eleringResponse.data.data.ee) {
+        final.priceData = eleringFormatter(eleringResponse.data.data.ee);
+      }
+
+      res.writeHead(200);
+      res.end(JSON.stringify(final));
+
+    })
+    .catch(function(error) {
+      console.log(error);
+      res.writeHead(400);
+      res.end({ status: "e2" });
+
+    });
+
+}
+
+const server = http.createServer(requestListener);
+server.listen(3335);
